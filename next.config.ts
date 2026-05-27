2fd3e7022fee657462408a6058fb135863f0e9d2
falakhe <fsivela@fragomen.com>
2026-05-27 22:08:45 +0200

    fix: scrollbar in datatable

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
+const API_PROXY_URL = process.env.API_PROXY_URL ?? 'http://127.0.0.1:8000';
+
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: '/api-proxy/:path*',
-        destination: 'http://127.0.0.1:8000/:path*',
+        destination: `${API_PROXY_URL}/:path*`,
       },
     ]
   },
